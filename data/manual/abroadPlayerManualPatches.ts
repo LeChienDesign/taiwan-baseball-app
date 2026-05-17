3c5db635769470067f0c2c11c8f727bf668a6e2b
Jason <yaojason2023@gmail.com>
2026-05-17 22:18:08 +0800

    Add manual photo keys for abroad players

--- data/manual/abroadPlayerManualPatches.ts
+++ data/manual/abroadPlayerManualPatches.ts
@@ -9,13 +9,97 @@ export type AbroadPlayerManualPatch = {
 };
 
 export const abroadPlayerManualPatches: Record<string, AbroadPlayerManualPatch> = {
-  'huang-chung-hsiang': {
-    photoKey: 'chung-hsiang-huang',
+  'an-ko-lin': {
+    photoKey: 'an-ko-lin',
+  },
+  'chia-cheng-lin': {
+    photoKey: 'chia-cheng-lin',
+  },
+  'chia-hao-song': {
+    photoKey: 'chia-hao-song',
   },
   'chia-hao-sung': {
     photoKey: 'chia-hao-song',
   },
   'sung-chia-hao': {
     photoKey: 'chia-hao-song',
+  },
+  'chih-jung-liu': {
+    photoKey: 'chih-jung-liu',
+  },
+  'chen-wei-lin': {
+    photoKey: 'chen-wei-lin',
+  },
+  'chen-zhong-ao-zhuang': {
+    photoKey: 'chen-zhong-ao-zhuang',
+  },
+  'ching-hsien-ko': {
+    photoKey: 'ching-hsien-ko',
+  },
+  'chun-wei-chang': {
+    photoKey: 'chun-wei-chang',
+  },
+  'chung-hsiang-huang': {
+    photoKey: 'chung-hsiang-huang',
+  },
+  'huang-chung-hsiang': {
+    photoKey: 'chung-hsiang-huang',
+  },
+  'corbin-carroll': {
+    photoKey: 'corbin-carroll',
+  },
+  'hao-yu-lee': {
+    photoKey: 'hao-yu-lee',
+  },
+  'hung-ling-chang': {
+    photoKey: 'hung-ling-chang',
+  },
+  'jo-hsi-hsu': {
+    photoKey: 'jo-hsi-hsu',
+  },
+  'jonathon-long': {
+    photoKey: 'jonathon-long',
+  },
+  'kai-wei-teng': {
+    photoKey: 'kai-wei-teng',
+  },
+  'po-yu-chen': {
+    photoKey: 'po-yu-chen',
+  },
+  'ruei-yang-gu-lin': {
+    photoKey: 'ruei-yang-gu-lin',
+  },
+  'sheng-en-lin': {
+    photoKey: 'sheng-en-lin',
+  },
+  'shosei-hsu': {
+    photoKey: 'shosei-hsu',
+  },
+  'hsiang-sheng-hsu': {
+    photoKey: 'shosei-hsu',
+  },
+  'stuart-fairchild': {
+    photoKey: 'stuart-fairchild',
+  },
+  'tsung-che-cheng': {
+    photoKey: 'tsung-che-cheng',
+  },
+  'tzu-chen-sha': {
+    photoKey: 'tzu-chen-sha',
+  },
+  'wei-en-lin': {
+    photoKey: 'wei-en-lin',
+  },
+  'wen-hui-pan': {
+    photoKey: 'wen-hui-pan',
+  },
+  'yen-cheng-wang': {
+    photoKey: 'yen-cheng-wang',
+  },
+  'yi-lei-sun': {
+    photoKey: 'yi-lei-sun',
+  },
+  'yu-min-lin': {
+    photoKey: 'yu-min-lin',
   },
 };
